d699a06b4a122aaf99b12431077061233d99ae0a
Ismael Garrido Muñoz <ismaelgarridomunoz@gmail.com>
2021-08-28 11:29:26 +0200

    helpers

--- src/FileHelper.py
+++ src/FileHelper.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import openpyxl
+from os import listdir
+from os.path import isdir
+from os.path import isfile, join
+
 
 def save_array_as_excel(data, fname):
     df = pd.DataFrame.from_records(data)
@@ -9,9 +13,11 @@ def save_array_as_excel(data, fname):
     writer.save()
     print('Fichero guardado en ' + path)
 
+
 def pandad_read_tsv(file):
     data = pd.read_csv(file, sep='\t', decimal=",")
     return data
+
 
 def write_log(text, fname):
     path = "results/test/" + fname
@@ -21,3 +27,13 @@ def write_log(text, fname):
     f.close()
 
     print('Fichero guardado en ' + path)
+
+
+def get_file_list(path):
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    return files
+
+
+def get_folder_list(path):
+    folders = [f for f in listdir(path) if isdir(join(path, f))]
+    return folders
