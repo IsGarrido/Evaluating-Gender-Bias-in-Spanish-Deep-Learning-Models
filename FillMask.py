ae34e226d17571c857cb840baf7a6ce252807a55
Ismael Garrido Muñoz <ismaelgarridomunoz@gmail.com>
2021-09-06 21:37:32 +0200

    save

--- FillMask.py
+++ FillMask.py
@@ -26,7 +26,6 @@ adjetivos_categorizados = read_lines_as_col_excel_asdict("../TextTools/Categoria
 all_adjectives = []
 run_id = 0
 
-
 # tokenizer = AutoTokenizer.from_pretrained("dccuchile/bert-base-spanish-wwm-uncased")
 # model = AutoModelForMaskedLM.from_pretrained("dccuchile/bert-base-spanish-wwm-uncased")
 
@@ -136,6 +135,10 @@ def save_run(model_name, points, kind="m"):
     l = []
     l_adj = []
     category_count = {}
+    category_points = {}
+
+    category_points["[TOTAL]"] = 0
+    category_count["[TOTAL]"] = 0
 
     for key, val in points.items():
         l.append(key + T + str(val))
@@ -144,18 +147,50 @@ def save_run(model_name, points, kind="m"):
         if key in adjectives_map:
             l_adj.append(key + T + str(val))
 
-        # Conteo de categorias
-        if key in adjetivos_categorizados:
-            category = adjetivos_categorizados[key]
+            # Buscar la categoria
+            category = '[???]'
+            if key in adjetivos_categorizados:
+                category = adjetivos_categorizados[key]
+
+            # Conteo de categorias, solo para los adjetivos encontrados
             if category in category_count:
                 category_count[category] = category_count[category]+1
             else:
                 category_count[category] = 0
 
+            category_count["[TOTAL]"] = category_count["[TOTAL]"] +1
+
+            # Suma de los puntos
+            if category in category_points:
+                category_points[category] = category_points[category] + val
+            else:
+                category_points[category] = val
+
+            category_points["[TOTAL]"] = category_points["[TOTAL]"] + val
+
 
     l_category = []
+    for category_key in category_count.keys():
+
+        category_val = category_count[category_key]
+
+        count = str(category_val)
+        prc_count = str( (category_val*100) / category_count["[TOTAL]"] )
+
+        points = category_points[category_key]
+        total_points =  category_points["[TOTAL]"]
+        prc_points = str( (points*100) /  total_points)
+
+        l_category.append(category_key + T + count+  T + prc_count + T + str(points) + T + prc_points )
+
+    '''
     for category_key, category_total in category_count.items():
-        l_category.append(category_key + T + category_total)
+
+        count = str(category_total)
+        prc_count = str( (category_total*100) / category_count["[TOTAL]"] )
+
+        l_category.append(category_key + T + count+  T + prc_count + T + T )
+    '''
 
     # Juntar lineas
     data = "\n".join(l)
@@ -163,7 +198,7 @@ def save_run(model_name, points, kind="m"):
     data_category = "\n".join(l_category)
 
     # Pasar a disco
-    path = RESULT_PATH + "/run_" + run_id + "_" + kind + "_" + as_file_name(model_name)
+    path = RESULT_PATH + "/run_" + str(run_id) + "_" + kind + "_" + as_file_name(model_name)
 
     write_txt(data, path + ".txt")
     write_txt(data_adj, path + "_adj.txt")
@@ -210,19 +245,27 @@ def run(modelname, tokenizername, MASK, DOT="."):
 
 
 # MARIA - BNE
+run_id = 1
 run("BSC-TeMU/roberta-base-bne", "BSC-TeMU/roberta-base-bne", "<mask>")
+
+run_id = 2
 run("BSC-TeMU/roberta-large-bne", "BSC-TeMU/roberta-large-bne", "<mask>")
 
 # BETO
+run_id = 3
 run("dccuchile/bert-base-spanish-wwm-uncased", "dccuchile/bert-base-spanish-wwm-uncased", "[MASK]")
+run_id = 4
 run("dccuchile/bert-base-spanish-wwm-cased", "dccuchile/bert-base-spanish-wwm-cased", "[MASK]")
 
+run_id = 5
 # https://huggingface.co/mrm8488 OSCAR
 run("mrm8488/electricidad-base-generator", "mrm8488/electricidad-base-generator", "[MASK]")
 
+run_id = 6
 # https://huggingface.co/MMG/mlm-spanish-roberta-base?text=MMG+se+dedica+a+la+%3Cmask%3E+artificial.
 run("MMG/mlm-spanish-roberta-base", "MMG/mlm-spanish-roberta-base", "<mask>")
 
+run_id = 7
 # BERTIN  https://huggingface.co/mrm8488
 run("bertin-project/bertin-roberta-base-spanish", "bertin-project/bertin-roberta-base-spanish", "<mask>")
 
